590673a109cb6d9566ccdb5b80da08b8eb088356
Caden1823 <1773875254@qq.com>
2022-07-06 16:26:20 +0800

    完成查询功能

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("查询用户");
-
+        System.out.println("今天是7月6号");
         System.out.println("登录");
     }
 }
